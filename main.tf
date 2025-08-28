253360d38af15a40c00e1172be8998bafaeeb4d8
RodrigoVillacinda <ligoche13@gmail.com>
2025-08-28 16:35:21 -0600

    Fix: use declared variables in use

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 ##################################################################################
 
 provider "aws" {
-  region = "us-east-1" # lo dejamos literal para no tocar variables.tf
+  region = var.aws_region
 }
 
 ##################################################################################
